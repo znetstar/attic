5c8db11b3ee25f12cb46398dbfa73e5b4214be71
Zachary Boyd <zachary@zacharyboyd.nyc>
2021-03-25 12:17:12 -0400

    add buildspec

--- attic-server/src/launch.ts
+++ attic-server/src/launch.ts
@@ -7,6 +7,7 @@ import {loadPlugins} from "./Plugins";
 
 (async () => {
     try {
+        process.stdin.resume();
         await ApplicationContext.emitAsync('launchStart');
         await loadPlugins();
         await loadModels();
