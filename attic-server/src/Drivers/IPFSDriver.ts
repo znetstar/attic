647415a2c5ff5533348b061606b7db9e6185b400
Zachary Boyd <zachary@zacharyboyd.nyc>
2021-10-14 15:27:16 -0400

    fixes typo

--- attic-server/src/Drivers/IPFSDriver.ts
+++ attic-server/src/Drivers/IPFSDriver.ts
@@ -12,6 +12,7 @@ import {HTTPDriverBase} from "./HTTPDriverBase";
 import {create, IPFSHTTPClient, Options as IPFSOptions} from 'ipfs-http-client';
 import ApplicationContext from "../ApplicationContext";
 import {HTTPResourceEntity} from "../Entities";
+import URL from 'url';
 
 export default class IPFSDriver extends Driver<IHTTPResponse> {
   public get ipfsClient(): IPFSHTTPClient {
@@ -36,55 +37,31 @@ export default class IPFSDriver extends Driver<IHTTPResponse> {
     return IPFSDriver.connect(this.options);
   }
 
+
   public async head(location: ILocation&Document): Promise<IHTTPResponse> {
-    const path = location.pathname.replace(this.basePathname, '');
-    let iterator: any;
-    let buf: Buffer[] = [];
-    const files: any[] = [];
-    let contentType = null;
-    if (path[path.length - 1] === '/') {
-      contentType = 'application/json';
-      iterator = await this.ipfsClient.ls(path.substr(0, path.length - 1));
-      for await (const file of iterator) {
-        files.push(file);
-      }
-      buf.push(Buffer.from(JSON.stringify(files), 'utf8'));
-    } else {
-      iterator = await this.ipfsClient.get(path);
-      for await (const chunk of iterator) {
-        buf.push(chunk);
-      }
-    }
-    const body = Buffer.concat(buf);
-    try {
-      const mmm = require('mmmagic'),
-        Magic = mmm.Magic;
-      const magic = new Magic(mmm.MAGIC_MIME_TYPE);
-      contentType = await new Promise<string>((resolve, reject) => {
-        magic.detectFile(body, function (err: any, result: any) {
-          if (err) reject(err);
-          else resolve(result);
-        });
-      }) || contentType;
-    } catch (err) {
-    }
-    return {
-      headers: new Map<string,string>([
-        ...contentType ? [ [ 'Content-Type', contentType ] ] : ([] as any[]),
-        [ 'Content-Length', body.byteLength.toString() ]
-      ]),
-      method: 'HEAD',
-      status: !body.byteLength ? 404 :  200,
-      href: location.href
-    }
+    const doc = await this.getHead(location);
+
+    delete doc.body;
+    return doc;
   }
 
-  public async get(location: ILocation&Document   ): Promise<IHTTPResponse> {
+  public async get(location: ILocation&Document): Promise<IHTTPResponse> {
+    return this.getHead(location);
+  }
+
+  protected async getHead(location: ILocation&Document): Promise<IHTTPResponse> {
     const path = location.pathname.replace(this.basePathname, '');
     let iterator: any;
     let buf: Buffer[] = [];
     const files: any[] = [];
-    let contentType = null;
+    let options: any = { archive: false };
+    const q = new URL.URL(location.href);
+    let contentType = 'application/octet-stream';
+    for (let [k,v] of Array.from(q.searchParams.entries())) {
+      options[k] = JSON.parse(v);
+    }
+    if (options.archive)
+      contentType = 'application/gzip'
     if (path[path.length - 1] === '/') {
       contentType = 'application/json';
       iterator = await this.ipfsClient.ls(path.substr(0, path.length - 1));
@@ -93,9 +70,21 @@ export default class IPFSDriver extends Driver<IHTTPResponse> {
       }
       buf.push(Buffer.from(JSON.stringify(files), 'utf8'));
     } else {
-      iterator = await this.ipfsClient.get(path);
-      for await (const chunk of iterator) {
-        buf.push(chunk);
+      iterator = await this.ipfsClient.ls(path);
+      for await (const file of iterator) {
+        if (file.type === 'file') {
+          iterator = await this.ipfsClient.cat(path, options);
+          for await (const chunk of iterator) {
+            buf.push(chunk);
+          }
+        } else if (file.type === 'dir') {
+          iterator = await this.ipfsClient.get(path, options);
+          for await (const chunk of iterator) {
+            buf.push(chunk);
+          }
+        }
+
+        break;
       }
     }
     const body = Buffer.concat(buf);
