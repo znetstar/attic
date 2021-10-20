11f08f5dd234071dd789edf2f49db3cdc3b065fc
maharit108 <maha.rit108@gmail.com>
2021-10-20 13:15:30 -0500

    discover page

--- pages/discover.tsx
+++ pages/discover.tsx
@@ -7,8 +7,10 @@ import {withRouter} from "next/router";
 import {IListedNFT, NFT, nftAcl, nftPubFields} from "./common/_nft";
 import {toPojo} from "@thirdact/to-pojo";
 import {getUser} from "./api/auth/[...nextauth]";
+import { MarketplaceAvatar } from "./common/_avatar";
 import { TextField, InputAdornment } from "@mui/material/";
 import SearchIcon from '@mui/icons-material/Search';
+import {NavBar} from "./common/_footer-nav";
 
 import styles from './../styles/discover.module.css';
 
@@ -32,11 +34,18 @@ export class Discover extends SessionComponent<DiscProps, DiscState> {
   }
 
   render() {
-    const displayNft = this.props.nfts ? this.props.nfts.filter(nft =>  nft.tags.join(' ').toLowerCase().includes(this.state.searchText) || nft.name.toLowerCase().includes(this.state.searchText)) : []
+    const displayNft = this.props.nfts ? 
+      this.props.nfts.filter(nft =>  
+        nft.tags.join(' ').toLowerCase().includes(this.state.searchText) || 
+        nft.name.toLowerCase().includes(this.state.searchText) || 
+        nft.sellerInfo.firstName.toLowerCase().includes(this.state.searchText) || 
+        nft.sellerInfo.lastName.toLowerCase().includes(this.state.searchText)) 
+      : []
+      console.log(displayNft)
     return (
     <div className={"page createNFT"}>
       {this.errorDialog}
-      {this.makeAppBar(this.props.router, 'Discover')}
+      {this.makeAppBar(this.props.router, 'DISCOVERY')}
       <TextField onChange={(e) => this.setState({ searchText: e.target.value.toLowerCase() })} 
                  variant={"outlined"} 
                  name={"priceStart"}  
@@ -44,20 +53,33 @@ export class Discover extends SessionComponent<DiscProps, DiscState> {
                  InputProps={{endAdornment: <InputAdornment position="end"><SearchIcon/></InputAdornment>, classes: { root: styles.input},}} />
       
         <div className={styles.nftWrapper}>
-          {displayNft ? displayNft.map(nft => (
+          {displayNft ? displayNft.map(nft => {
+            const sellerImg = nft.sellerInfo.image ? nft.sellerInfo.image : ''
+            const sellerFirstName = nft.sellerInfo.firstName ? nft.sellerInfo.firstName : ''
+            const sellerLastName = nft.sellerInfo.lastName ? nft.sellerInfo.lastName : ''
+            return(
             <div className={styles.nft} key={nft._id}>
               <div className={styles.nftImg}>{nft.image}</div>
               <div className={styles.nftInfo}>
                 <div className={styles.name}>{nft.name}</div>
                 <div className={styles.owner}>
-                  <div className={styles.ownerImg}></div>
-                  <div className={styles.ownerName}></div>
+                  <div className={styles.ownerImg}>
+                    <MarketplaceAvatar
+                      image={
+                        typeof(sellerImg) === 'string' ? Buffer.from(sellerImg, 'base64') : sellerImg
+                      }
+                      imageFormat={this.enc.options.imageFormat}
+                      allowUpload={false}
+                      resizeImage={{height:30, width:30}}
+                    ></MarketplaceAvatar>
+                  </div>
+                  <div className={styles.ownerName}>{sellerFirstName && sellerLastName ? '@' + sellerFirstName + '_' + sellerLastName : '@' + sellerFirstName + sellerLastName}</div>
                 </div>
               </div>
             </div>
-          )) : 'Please check later!!!'}
+          )}) : 'Please check later!!!'}
         </div>
-
+      <NavBar session={this.props.session}/>
     </div>);
   }
 }
