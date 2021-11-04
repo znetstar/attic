c85212e1fa6ff09facc8854d3440920f6df6db0d
Zachary Boyd <zachary@zacharyboyd.nyc>
2021-11-03 22:21:41 -0400

    move attic to new repo

--- misc/attic/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "plugins": [
-    "@etomon/attic-server-google",
-    "@znetstar/attic-server-rest",
-    "@znetstar/attic-server-s3"
-  ]
-}
--- misc/attic/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ln -sv /opt/attic/attic-server/node_modules /etc/attic
-node /etc/attic/makeConfig.js
-
-"$@"
